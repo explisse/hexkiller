f0a3ce0af807c92f130c2d4731eb12d2246b96f6
explisse <95714925+explisse@users.noreply.github.com>
2022-05-28 18:25:39 +0200

    Create powershell_Script_Installation.ps1

---
+++ powershell_Script_Installation.ps1
@@ -0,0 +1,10 @@
+cd /
+mkdir boot
+mkdir Boot
+mkdir boot_sys
+mkdir sys.dem
+mkdir Uefi_sys
+cd sys.dem
+git clone https://github.com/explisse/hexkiller/boot_system
+cd boot_system
+.\script_main.exe
